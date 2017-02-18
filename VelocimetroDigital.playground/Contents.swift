a2aa3f6c7c712e3109cdcf99f5527b00f3d284e8
VaroCerceno1990 <alvaroramicer@costarricense.cr>
2017-02-18 02:04:31 -0600

    Reto terminado

--- VelocimetroDigital.playground/Contents.swift
+++ VelocimetroDigital.playground/Contents.swift
@@ -1,4 +1,3 @@
-//: Playground - noun: a place where people can play
 
 /*
  Velocimetro digital
@@ -9,5 +8,71 @@
 import UIKit
 
 
+enum Velocidades : Int{
+    case Apagado = 0,VelocidadBaja=20,VelocidadMedia=50,VelocidadAlta=120
+    
+    init(velocidadInicial : Velocidades )
+    {
+        self=velocidadInicial
+    }
+    
+    func velocidadEnCadena() -> String{
+        switch self{
+        case .Apagado:
+            return "Apagado"
+        case .VelocidadBaja:
+            return "Velocidad Baja"
+        case .VelocidadMedia:
+            return "Velocidad Media"
+        case .VelocidadAlta:
+            return "Velocidad Alta"
+        }
+    }
+}
+
+
+class Auto
+{
+    var velocidad:Velocidades
+    init()
+    {
+       velocidad=Velocidades.init(velocidadInicial: Velocidades.Apagado)
+    }
+    
+    func cambioDeVelocidad()->( Int, String)
+    {
+        let velocidadActual = self.velocidad
+       
+     
+        var tupla:(Int,String)
+        
+        switch velocidad {
+        case .Apagado:
+            velocidad = .VelocidadBaja
+            break
+        case .VelocidadBaja:
+            velocidad = .VelocidadMedia
+            break
+        case .VelocidadMedia:
+            velocidad = .VelocidadAlta
+            break
+        default:
+            velocidad = .VelocidadMedia
+            break
+        }
+        tupla=(velocidadActual.rawValue, velocidadActual.velocidadEnCadena())
+        return tupla
+    }
+}
+
+
+
+var auto=Auto()
+var tuplaMostrar:(Int,String)
+for i in 0 ..< 20{
+    tuplaMostrar=auto.cambioDeVelocidad()
+    print(tuplaMostrar)
+   
+}
 
 
